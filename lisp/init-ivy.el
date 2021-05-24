e8bfba63e76b94e00a2f360e49943047b5b18529
Gilberto Diaz <gilbertoabacusinsights.com@AI-US-MAC-GD-01.attlocal.net>
2021-05-24 08:58:41 -0500

    Change order in init-ivy

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -3,14 +3,14 @@
   :bind (("C-s" . swiper))
   :config (ivy-mode 1)) ; ---------------------------- Better search interface
 
-(use-package ivy-rich
-  :init (ivy-rich-mode 1)) ; -------------------------- Short documentation when serching commands
-
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
 	 ("C-x C-f" . counsel-find-file)
 	 ("C-x b" . counsel-ibuffer)
 	 :map minibuffer-local-map
 	 ("C-r" . 'counsel-minibuffer-history))) ; --- Rich interface for finding commands/files
+
+(use-package ivy-rich
+  :init (ivy-rich-mode 1)) ; ------------------------- Short documentation when serching commands
 
 (provide 'init-ivy)
