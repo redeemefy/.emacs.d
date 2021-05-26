c822d4a5e6388653c4e1083ac840788def19ab8e
Gilberto Diaz <gilbertodiaz@Gilbertos-MBP.attlocal.net>
2021-05-25 21:34:33 -0500

    Refactored for better keybindings with evil-leader

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;; init.el --- Loads the full emacs configuration
+;;; init.el --- Bootstrap EMACS
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory)) ; --- Load the lisp directory
 (require 'init-package) ; --------------------------------------------------- Resources elpa/melpa/etc
@@ -12,16 +12,3 @@
 (require 'init-evil) ; ------------------------------------------------------ Vim inside emacs
 (require 'init-which-key) ; ------------------------------------------------- Key completion in status bar window
 (require 'init-helpful) ; --------------------------------------------------- Better documentation when using describe
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(general which-key use-package rainbow-delimiters ivy-rich helpful evil doom-themes doom-modeline counsel)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -1,16 +1,20 @@
-(use-package evil-leader
+(use-package evil-leader ; ----------------------------------- Set your VIM leader key
   :config
   (global-evil-leader-mode)
-  (evil-leader/set-leader "<SPC>")
+  (evil-leader/set-leader "<SPC>") ; ------------------------- Spacebar as leader key
   (evil-leader/set-key
-    "e" 'counsel-find-file
-    "b" 'counsel-switch-buffer
-    "k" 'kill-buffer
-    "i" 'swiper
-    "z" 'shell
-    "s" 'save-buffer))
+    "e" 'counsel-find-file ; --------------------------------- Default file search
+    "cff" 'counsel-fzf ; ------------------------------------- Recursive search by file name with fuzzy matching
+    "crg" 'counsel-rg ; -------------------------------------- Recursive search by string content with ripgrep
+    "csb" 'counsel-switch-buffer ; --------------------------- List buffers with counsel
+    "cdf" 'counsel-describe-function ; ----------------------- Describe functions with counsel
+    "cdv" 'counsel-describe-variable ; ----------------------- Describe variables with counsel
+    "k" 'kill-buffer ; --------------------------------------- Default kill buffer
+    "i" 'swiper ; -------------------------------------------- Isearch current buffer with swiper
+    "z" 'shell ; --------------------------------------------- Default open shell buffer
+    "s" 'save-buffer)) ; ------------------------------------- Default save current buffer
 
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit) ; ------------------- TODO: Move this to init-key-bindings
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit) ; ---- Get out of mini-buffer command such as C-x/M-x
 
 
 (provide 'init-keybindings)
