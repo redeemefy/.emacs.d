4860869db8c16f9df67aec03a3aa9fe2de7ca07d
Gilberto Diaz <gilbertodiaz@Gilbertos-MBP.attlocal.net>
2021-05-26 09:10:17 -0500

    Modify keybindings'

--- init.el
+++ init.el
@@ -12,3 +12,16 @@
 (require 'init-evil) ; ------------------------------------------------------ Vim inside emacs
 (require 'init-which-key) ; ------------------------------------------------- Key completion in status bar window
 (require 'init-helpful) ; --------------------------------------------------- Better documentation when using describe
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(which-key use-package rainbow-delimiters ivy-rich helpful general evil-leader doom-themes doom-modeline counsel)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
