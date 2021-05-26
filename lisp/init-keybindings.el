a9e4d7cd2a21f10bb669730bc7061863660e65ee
Gilberto Diaz <gilbertodiaz@Gilbertos-MBP.attlocal.net>
2021-05-26 09:18:01 -0500

    Aligned comments

--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -3,16 +3,16 @@
   (global-evil-leader-mode)
   (evil-leader/set-leader "<SPC>") ; ------------------------- Spacebar as leader key
   (evil-leader/set-key
-    "cff" 'counsel-find-file ; --------------------------------- Default file search
+    "cff" 'counsel-find-file ; ------------------------------- Default file search
     "cfz" 'counsel-fzf ; ------------------------------------- Recursive search by file name with fuzzy matching
     "crg" 'counsel-rg ; -------------------------------------- Recursive search by string content with ripgrep
     "csb" 'counsel-switch-buffer ; --------------------------- List buffers with counsel
     "cdf" 'counsel-describe-function ; ----------------------- Describe functions with counsel
     "cdv" 'counsel-describe-variable ; ----------------------- Describe variables with counsel
-    "kb" 'kill-buffer ; --------------------------------------- Default kill buffer
+    "kb" 'kill-buffer ; -------------------------------------- Default kill buffer
     "i" 'swiper ; -------------------------------------------- Isearch current buffer with swiper
-    "zsh" 'shell ; --------------------------------------------- Default open shell buffer
-    "sb" 'save-buffer)) ; ------------------------------------- Default save current buffer
+    "zsh" 'shell ; ------------------------------------------- Default open shell buffer
+    "sb" 'save-buffer)) ; ------------------------------------ Default save current buffer
 
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit) ; ---- Get out of mini-buffer command such as C-x/M-x
 
